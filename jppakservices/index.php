9320799175ebf3c08cf8e27e29bc03cae4a8445b
Muhammad Suhaib <suhaibkhan@msn.com>
2017-12-26 14:50:44 +0900

    change in this file

---
+++ jppakservices/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Japan Pakistan Services Provider</title>
+    <meta name="description" content="website description" />
+    <meta name="keywords" content="website keywords, website keywords" />
+    <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="index.php">Japan Pakistan Services<span class="logo_colour">Provider</span></a></h1>
+          <h2>Education For All.</h2>
+        </div>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+          <li class="selected"><a href="index.php">Home</a></li>
+          <li><a href="studyjapan.php">Study in Japan</a></li>
+          <li><a href="another_page.html">Another Page</a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="content_header"></div>
+    <div id="site_content">
+      <div id="banner"></div>
+
+
+
+
+
+	  <div id="sidebar_container">
+        <div class="sidebar">
+          <div class="sidebar_top"></div>
+          <div class="sidebar_item">
+            <!-- insert your sidebar items here -->
+            <h3>Latest News</h3>
+            <h4>Admission Open for July 2018 Intake</h4>
+            <h5> Deadline Jan 30, 2017</h5>
+            <p></p>
+          </div>
+          <div class="sidebar_base"></div>
+        </div>
+        <div class="sidebar">
+          <div class="sidebar_top"></div>
+          <div class="sidebar_item">
+            <h3>Useful Links</h3>
+            <ul>
+              <li><a href="#">link 1</a></li>
+              <li><a href="#">link 2</a></li>
+              <li><a href="#">link 3</a></li>
+              <li><a href="#">link 4</a></li>
+            </ul>
+          </div>
+          <div class="sidebar_base"></div>
+        </div>
+        <div class="sidebar">
+          <div class="sidebar_top"></div>
+          <div class="sidebar_item">
+            <h3>Search</h3>
+            <form method="post" action="#" id="search_form">
+              <p>
+                <input class="search" type="text" name="search_field" value="Enter keywords....." />
+                <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="style/search.png" alt="Search" title="Search" />
+              </p>
+            </form>
+          </div>
+          <div class="sidebar_base"></div>
+        </div>
+      </div>
+      <div id="content">
+        <!-- insert the page content here -->
+        <h1>Japan Pakistan Services Provider</h1>
+          <p>It is all due the effort commitment of Japan Pakistan Services Provider CEO Engr.Muhammad Suhaib, who earned an Master of Engineering Major in Computer System & Software Engineering from Japan, to maintain parents and student’s reliability and trust.</p>
+
+
+        <p>We are glad to welcome you to JPSP website! It is rather content-rich and provides you with tons of information on all study in JAPAN and university admissions matters and business setup in JAPAN.<br> <br> </p>
+        <p>test</a>.</p>
+          <p> test </p>
+
+
+      </div>
+    </div>
+    <div id="content_footer"></div>
+    <div id="footer">
+      <p><a href="index.html">Home</a> | <a href="examples.html">Examples</a> | <a href="page.html">A Page</a> | <a href="another_page.html">Another Page</a> | <a href="contact.html">Contact Us</a></p>
+      <p>Copyright &copy; textured_orbs | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">Website templates</a></p>
+    </div>
+  </div>
+</body>
+</html>
---
+++ jppakservices/studyjapan.php
@@ -0,0 +1,244 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Japan Pakistan Services Provider</title>
+    <meta name="description" content="website description" />
+    <meta name="keywords" content="website keywords, website keywords" />
+    <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="index.php">Japan Pakistan Services<span class="logo_colour">Provider</span></a></h1>
+          <h2>Education For All.</h2>
+        </div>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+          <li><a href="index.php">Home</a></li>
+          <li class="selected"><a href="studyjapan.php">Study in Japan</a></li>
+          <li><a href="another_page.html">Business Visa</a></li>
+            <li><a href="another_page.html">Japanese Student Visa </a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="content_header"></div>
+    <div id="site_content">
+      <div id="sidebar_container">
+        <div class="sidebar">
+          <div class="sidebar_top"></div>
+          <div class="sidebar_item">
+            <!-- insert your sidebar items here -->
+            <h3>Latest News</h3>
+            <h4>Admission Open for July 2018 Intake</h4>
+            <h5>Feb 1st, 2018</h5>
+            <p></p>
+          </div>
+          <div class="sidebar_base"></div>
+        </div>
+        <div class="sidebar">
+          <div class="sidebar_top"></div>
+          <div class="sidebar_item">
+            <h3>Useful Links</h3>
+            <ul>
+              <li><a href="#">Study in Japan</a></li>
+              <li><a href="#">Japan Immigration</a></li>
+              <li><a href="#">Highly Skilled Visa</a></li>
+
+            </ul>
+          </div>
+          <div class="sidebar_base"></div>
+        </div>
+        <div class="sidebar">
+          <div class="sidebar_top"></div>
+          <div class="sidebar_item">
+            <h3>Search</h3>
+            <form method="post" action="#" id="search_form">
+              <p>
+                <input class="search" type="text" name="search_field" value="Enter keywords....." />
+                <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="style/search.png" alt="Search" title="Search" />
+              </p>
+            </form>
+          </div>
+          <div class="sidebar_base"></div>
+        </div>
+      </div>
+      <div id="content">
+        <!-- insert the page content here -->
+
+
+          <h2>General Admission Requirements <br> Japanese Language Program</h2>
+
+          <table style="width:100%; border-spacing:0;">
+              <tr><th>Required Documents</th><th>Description</th></tr>
+              <tr><td>Application Sheet</td><td>Must be printed out</td></tr>
+              <tr><td>Statement of Reason </td><td>The reason for applying to the desired program or course. <br>(Approx. 400 words)</td></tr>
+              <tr><td>Application Fee</td><td>20-50 USD</td></tr>
+              <tr><td>Tuition Fee</td><td> 3800 USD 4500  to Per Semester</td></tr>
+              <tr><td>Admission Fee</td><td>300 USD One Time Only</td></tr>
+              <tr><td>Scholarships</td><td>   Available After Enrollment          </td></tr>
+              <tr><td>IELTS</td><td>Not Required </td></tr>
+              <tr><td>Bank Statement Requirement</td><td> 20 Lacs Minimum </td></tr>
+              <tr><td>Note</td><td>All Payments Should be Transferred in Institute Account directly from student </td></tr>
+              <tr><td>Contact Us</td><td>WhatsApp: +81-702-8058-303, Email: jppakservices@gmail.com,<br> 203 3-40-6 Higashiyagura Kusatsu ShigaKen Japan <br> Dr Zubair Ahmed +92-335-3295120 Karachi Pakistan</td></tr>
+
+          </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <h2>General Admission Requirements <br> Bachelor Information Science and Engineering Program</h2>
+
+        <table style="width:100%; border-spacing:0;">
+          <tr><th>Required Documents</th><th>Description</th></tr>
+          <tr><td>Application Sheet</td><td>Must be printed out</td></tr>
+          <tr><td>Statement of Reason </td><td>The reason for applying to the desired program or course. <br>(Approx. 400 words)</td></tr>
+            <tr><td>2 Letter of Reference</td><td> Two letters must be signed and sealed in an envelope by the referees who wrote
+                    it. The letter must be written in English or Japanese by a teacher at your school. The
+                    original must be submitted (photocopies are not allowed).
+                    If you have already graduated from high school and you cannot obtain a letter of
+                    reference from a teacher/a staff member of your school, a letter from your supervisor or
+                    manager at your place of employment would be acceptable. </td></tr>
+          <tr><td>Application Essay </td><td> (ISSE Essay 1&2: 300 words, ISSE Essay 3: 600 words)</td></tr>
+          <tr><td>Application Fee</td><td>75 USD</td></tr>
+          <tr><td>Tuition Fee</td><td> 6000 USD Per Semester</td></tr>
+          <tr><td>Admission Fee</td><td>3000 USD One Time Only</td></tr>
+
+<tr><td>Scholarships</td><td>   The recipients and amount of the tuition reduction scholarship will be determined based on the applicants screening
+                                 results.<br>
+                                 Undergraduate I 100%
+                                 Undergraduate II 50%
+                                 Undergraduate III 20%
+
+
+                                 </td></tr>
+          <tr><td>IELTS</td><td>Not Required /  Medium of Instruction Letter Required from High School & Secondary School</td></tr>
+          <tr><td>Note</td><td>All Payments Should be Transfered in University Account directly from student </td></tr>
+           <tr><td>Contact Us</td><td>WhatsApp: +81-702-8058-303, Email: jppakservices@gmail.com,<br> 203 3-40-6 Higashiyagura Kusatsu ShigaKen Japan</td></tr>
+
+        </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             <h4 class="secTtl">Eligibility for English Proficiency</h4>
+                               </dt>
+                               <dd>
+                                 <p>
+                                   In order to demonstrate sufficient English ability, applicants must take one of the tests listed below and achieve at least the minimum score indicated below by the time of application.
+               ( Applicants whose most recent six years of formal education before the enrollment date were conducted entirely in English do not need to submit the test scores listed above. However, the University may request evidence that education at the student’s school is delivered in English.) </p>
+                                 <div class="tblBody abl">
+                                   <table>
+                                     <thead>
+                                       <tr>
+                                         <th>TOEFL<sup>®</sup> PBT Test</th>
+                                         <th>TOEFL iBT<sup>®</sup> Test</th>
+                                         <th><p>IELTS</p><p>&nbsp;(Academic Module)</p></th>
+                                         <th><p>TOEIC<sup>®</sup></p><p>&nbsp;Listening&nbsp;and Writing Test</p></th>
+                                       </tr>
+                                     </thead>
+                                     <tbody>
+                                       <tr>
+                                         <td>530</td>
+                                         <td>71</td>
+                                         <td>5.5</td>
+                                         <td>730</td>
+                                       </tr>
+                                     </tbody>
+                                   </table>
+                                 </div>
+                                 <p class="small">
+                                   *The English tests must have been taken within the two-year period prior to the start date of the application period that the applicant applies for and the test score must be available during the application period.<br>
+
+
+                                     *The University does not accept institutional tests such as TOEFL ITP<sup>®</sup>Test or TOEIC<sup>®</sup> IP Test.<br>*TOEIC<sup>®</sup> and TOEFL<sup>®</sup> are registered trademarks of Educational Testing Service (ETS).<br></p></dd>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div id="content_footer"></div>
+    <div id="footer">
+      <p><a href="index.html">Home</a> | <a href="examples.html">Examples</a> | <a href="page.html">A Page</a> | <a href="another_page.html">Another Page</a> | <a href="contact.html">Contact Us</a></p>
+      <p>Copyright &copy; textured_orbs | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">Website templates</a></p>
+    </div>
+  </div>
+</body>
+</html>
